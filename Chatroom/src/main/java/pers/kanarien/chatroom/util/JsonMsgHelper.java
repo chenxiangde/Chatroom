3a00ac9599a23c93b6f1a4e8456b27e42c94eb1b
chenxiangde <xiangdechen@163.com>
2022-06-26 17:43:23 +0800

    代码格式化

--- Chatroom/src/main/java/pers/kanarien/chatroom/util/JsonMsgHelper.java
+++ Chatroom/src/main/java/pers/kanarien/chatroom/util/JsonMsgHelper.java
@@ -13,17 +13,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonMsgHelper {
 
-	private static final ObjectMapper JSONMAPPER = new ObjectMapper();
+    private static final ObjectMapper OBJECTMAPPER = new ObjectMapper();
 
-	public static void writeJson(HttpServletResponse response, Object pojo, HttpStatus status)
-			throws JsonGenerationException, JsonMappingException, IOException {
-		response.setContentType("application/json;charset=UTF-8");
-		response.setStatus(status.value());
-		writeJson(response.getWriter(), pojo);
-	}
+    public static void writeJson(HttpServletResponse response, Object pojo, HttpStatus status) throws JsonGenerationException, JsonMappingException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(status.value());
+        writeJson(response.getWriter(), pojo);
+    }
 
-	public static void writeJson(Writer writer, Object pojo)
-			throws JsonGenerationException, JsonMappingException, IOException {
-		JSONMAPPER.writeValue(writer, pojo);
-	}
+    public static void writeJson(Writer writer, Object pojo) throws JsonGenerationException, JsonMappingException, IOException {
+        OBJECTMAPPER.writeValue(writer, pojo);
+    }
 }
