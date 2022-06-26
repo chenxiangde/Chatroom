076869cee22d92c95cf7f01a5885353631d455f3
chenxiangde <xiangdechen@163.com>
2022-06-26 17:38:07 +0800

    代码格式化

--- Chatroom/src/main/java/pers/kanarien/chatroom/model/vo/ResponseJson.java
+++ Chatroom/src/main/java/pers/kanarien/chatroom/model/vo/ResponseJson.java
@@ -12,7 +12,7 @@ public class ResponseJson extends HashMap<String, Object> {
     private static final Integer SUCCESS_STATUS = 200;
     private static final Integer ERROR_STATUS = -1;
     private static final String SUCCESS_MSG = "一切正常";
-    
+
     public ResponseJson() {
         super();
     }
@@ -27,19 +27,19 @@ public class ResponseJson extends HashMap<String, Object> {
         setStatus(status.value());
         setMsg(status.getReasonPhrase());
     }
-    
+
     public ResponseJson success() {
         put("msg", SUCCESS_MSG);
         put("status", SUCCESS_STATUS);
         return this;
     }
-    
+
     public ResponseJson success(String msg) {
         put("msg", msg);
         put("status", SUCCESS_STATUS);
         return this;
     }
-    
+
     public ResponseJson error(String msg) {
         put("msg", msg);
         put("status", ERROR_STATUS);
@@ -56,7 +56,7 @@ public class ResponseJson extends HashMap<String, Object> {
         data.put(key, obj);
         return this;
     }
-    
+
     public ResponseJson setStatus(int status) {
         put("status", status);
         return this;
@@ -71,7 +71,7 @@ public class ResponseJson extends HashMap<String, Object> {
         put(key, val);
         return this;
     }
-    
+
     /**
      * 返回JSON字符串
      */
